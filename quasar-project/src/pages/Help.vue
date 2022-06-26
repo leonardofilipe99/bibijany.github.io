24c8c81facb0ebbcb6cfdb4ffa584a60406c2200
Leonardo J. M. Filipe <66243493+leonardofilipe99@users.noreply.github.com>
2022-06-26 20:32:24 +0200

    Initial commit

---
+++ quasar-project/src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help </h5>
+    <p>A configuração do leitor de tela é válida apenas os seguintes produtos: Documentos, Planilhas, Apresentações, Formulários e Desenhos Google em um navegador, como o Chrome. Quando você usa esses produtos, um leitor de tela pode ler em voz alta os textos exibidos na sua tela.</p>
+        <p>A configuração do leitor de tela é válida apenas os seguintes produtos: Documentos, Planilhas, Apresentações, Formulários e Desenhos Google em um navegador, como o Chrome. Quando você usa esses produtos, um leitor de tela pode ler em voz alta os textos exibidos na sua tela.</p>
+ </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
